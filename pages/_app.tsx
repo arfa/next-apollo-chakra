2d40ca5e8cf2bf0dbddd650e7fb004f68af6040c
Marwen Arfa <marwen.arfa@gmail.com>
2022-02-28 07:27:56 +0100

    chore: setup apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://kf9p4bkih6.execute-api.eu-west-1.amazonaws.com/dev/',
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
